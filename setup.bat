a0ab5d453d1a1dbe3a55b4df419b160706c9032e
Justin Walsh <contact.me@thejustinwalsh.com>
2016-09-16 13:42:02 -0700

    Windows setup.bat
    Adding project generation helper for windows

--- setup.bat
+++ setup.bat
@@ -1,2 +1,21 @@
- echo TODO
- 
+pushd "%~dp0"
+
+REM Install premake
+if not exist ".premake" mkdir .premake
+pushd ".premake"
+
+REM Remove existing premake archive
+if exist "premake.zip" del /F/Q premake.zip
+
+REM Download premake
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/premake/premake-core/releases/download/v5.0.0-alpha9/premake-5.0.0-alpha9-windows.zip', 'premake.zip')"
+
+REM Extract premake
+powershell -Command "Expand-Archive -Path %cd%\premake.zip -DestinationPath %cd% -Force;"
+
+REM Run premake to generate our project
+premake5.exe --file=..\premake.lua vs2015
+
+popd
+
+popd
