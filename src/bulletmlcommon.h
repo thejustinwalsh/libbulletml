6b65c7e10f96e3a792fdbc1aea0bcbbcc515c6fd
Justin Walsh <contact.me@thejustinwalsh.com>
2016-09-16 20:04:07 -0700

    BULLETML_SHARED_LIB
    Explicitly require that we define BULLETML_SHARED_LIB when wanting to
    work with a DLL.

--- src/bulletmlcommon.h
+++ src/bulletmlcommon.h
@@ -1,13 +1,13 @@
 #ifndef bulletmlcommon_h_
 #define bulletmlcommon_h_
 
-#if defined(_MSC_VER)
-    #if defined(WIN32_DLL_EXPORT)
-        #define BULLETML_API __declspec(dllexport)
-    #else
-        #define BULLETML_API __declspec(dllimport)
-    #endif
-#elif defined(__GNUC__)
+#if defined(_MSC_VER) && defined(BULLETML_SHARED_LIB)
+	#if defined(WIN32_DLL_EXPORT)
+		#define BULLETML_API __declspec(dllexport)
+	#else
+		#define BULLETML_API __declspec(dllimport)
+	#endif
+#elif defined(__GNUC__) && defined(BULLETML_SHARED_LIB)
     #define BULLETML_API __attribute__((visibility("default")))
 #else
     #define BULLETML_API
