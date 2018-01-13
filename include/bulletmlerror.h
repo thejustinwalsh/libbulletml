688f10f3a2c73ef2a83eee1eb3e813e772a5cbbe
Brett Fowle <brettfowle@gmail.com>
2018-01-12 21:04:58 -0500

    bulletmlerror - use preferred styles, add english comments

--- include/bulletmlerror.h
+++ include/bulletmlerror.h
@@ -7,20 +7,25 @@
 #include <string>
 
 /// さすがにもうちょっと充実させんとねえ
-class BULLETML_API BulletMLError : public std::runtime_error
-{
+/// As expected, it is a little more fulfilling
+class BULLETML_API BulletMLError : public std::runtime_error {
 public:
-    BulletMLError(const std::string& msg) : std::runtime_error(msg) {}
-
-    static void doAssert(const char* str) { throw BulletMLError(str); }
-    static void doAssert(const std::string& str) { throw BulletMLError(str); }
-    static void doAssert(bool t, const char* str)
-    {
-        if (!t) throw BulletMLError(str);
+    BulletMLError(const std::string &msg) : std::runtime_error(msg) {
     }
-    static void doAssert(bool t, const std::string& str)
-    {
-        if (!t) throw BulletMLError(str);
+
+    static void doAssert(const char *str) { throw BulletMLError(str); }
+    static void doAssert(const std::string &str) { throw BulletMLError(str); }
+
+    static void doAssert(bool t, const char *str) {
+        if (!t) {
+            throw BulletMLError(str);
+        }
+    }
+    
+    static void doAssert(bool t, const std::string &str) {
+        if (!t) {
+            throw BulletMLError(str);
+        }
     }
 };
 
